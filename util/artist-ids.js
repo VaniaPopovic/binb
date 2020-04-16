c8af1fb6af25574e0c9dce2676f26f97394b6fae
Vanja Popovic <vanja@talkingmedicines.com>
2020-04-16 19:08:37 +0100

    lets try again

--- util/artist-ids.js
+++ util/artist-ids.js
@@ -6,89 +6,83 @@
 
 module.exports = {
   pop: [
-    398120, //NSYNC
-    65632953, //2UNLIMITED
-    105998, //2PAC
-    101604, // 3DD
-    2307416, //30stm
-    362923, //4nonblondes
-    538811449, //5secondof
-    112058, //50cent
-    1451667444, //atc
-    3249567, //arrahman
-    5040714, //acdc
-    13874, //ace of base
-    305371030, //adam lambert
-    262836961, // ADELE
-    115386, //aerosmith
-    211210469, // afrojack
-    88434, //afroman
-    42568985, //agnes
-    3261325, //akon
-    1062085272, //alan walker
-	1092859, //alanis
-	278007, //alcazar
-	60586856, //alesha dixon
-	991444375, //alessia cara
-	432464201, //alesso
-	404748613, //alexandra stan
-	80354341, //alice deejay
-	316069, //alicia keys
-	3446405, //alien ant farm
-	724744, //all saints
-	159019, //all for one
-	4750752, //aloe blacc
-	259959460, //amy mcdonald
-	13125609, //amy winehouse
-	85942, //anastacia
-	6059292, //andreas johnson
-	14000, //annie lenox
-	1646339, //apollodie
-	62978, //aqua
-	62820413, //artic monkeys
-	412778295, //ariana grande
-	17976294, //armin van buuren
-	13332559, //ashlee simpson
-	3089519, //atomic kitten
-	1265164818, //avamax
-	38495706, //aventura
-	298496035, //avicii
-	459885, //avril lavigne
-	//128864409, //axwell
-	259700865, //bob
-	526199085, //baauer
-	217039, //bsb
-	148485360,
-	50760, //bahamen
-	420203509, //bastille
-	800263205, //bazzi	 
-	466059563, //bebe rexha
-	312095, //beck
-	15654100, //benny benassi
-	1410841257, //benny blanco
-	1419227, //beyonce
-	889327, //shakira
-	509491, //big mountain
-	1065981054, //bilie eilish
-	1806816, //billy talent
-	290652, //blackstreet
-	116851, //blink182
-	//1012882, //blondie
-	861350, //bloodhoung gang
-	15726, //blu centrell
-	//BLUE ADD ME
-    528564, //BLUR
-     14606058, //Bob sinclar
-	//boomfunk mcs
-	122782, //bonjovi
-	256514, //boyz ii men
-	17416, //brandy
-	217005, //britney spears
-	84239286, //broke fraiser
-	  //bruce
-	278873078, //bruno mars
-	85932, //bryan adams
-	54583, //busta rymes
+398120,//*NSYNC
+65632953,//2 Unlimited
+105998,//2Pac
+101604,//3 Doors Down
+2307416,//Thirty Seconds to Mars
+362923,//4 Non Blondes
+538811449,//5 Seconds of Summer
+112058,//50 Cent
+1451667444,//A Touch of Class
+3249567,//A. R. Rahman
+5040714,//AC/DC
+13874,//Ace of Base
+305371030,//Adam Lambert
+262836961,//Adele
+115386,//Aerosmith
+211210469,//Afrojack
+88434,//Afroman
+42568985,//Agnes
+3261325,//Akon
+1062085272,//Alan Walker
+1092859,//Alanis Morissette
+278007,//Alcazar
+60586856,//Alesha Dixon
+991444375,//Alessia Cara
+432464201,//Alesso
+404748613,//Alexandra Stan
+80354341,//Alice Deejay
+316069,//Alicia Keys
+3446405,//Alien Ant Farm
+724744,//All Saints
+159019,//All-4-One
+4750752,//Aloe Blacc
+259959460,//Amy Macdonald
+13125609,//Amy Winehouse
+85942,//Anastacia
+6059292,//Andreas Johnson
+14000,//Annie Lennox
+1646339,//Apollo 440
+62978,//Aqua
+62820413,//Arctic Monkeys
+412778295,//Ariana Grande
+17976294,//Armin van Buuren
+13332559,//Ashlee Simpson
+3089519,//Atomic Kitten
+1265164818,//Ava Max
+38495706,//Aventura
+298496035,//Avicii
+459885,//Avril Lavigne
+259700865,//B.o.B
+526199085,//Baauer
+217039,//Backstreet Boys
+148485360,//Bad Meets Evil
+50760,//Baha Men
+420203509,//Bastille
+800263205,//Bazzi
+466059563,//Bebe Rexha
+312095,//Beck
+15654100,//Benny Benassi
+1410841257,//benny blanco
+1419227,//Beyoncé
+889327,//Shakira
+509491,//Big Mountain
+1065981054,//Billie Eilish
+1806816,//Billy Talent
+290652,//Blackstreet
+116851,//blink-182
+861350,//Bloodhound Gang
+528564,//Blur
+14606058,//Bob Sinclar
+122782,//Bon Jovi
+256514,//Boyz II Men
+17416,//Brandy
+217005,//Britney Spears
+84239286,//Brooke Fraser
+278873078,//Bruno Mars
+85932,//Bryan Adams
+54583,//Busta Rhymes
   ],
   rap: [
     1587965, // A Tribe Called Quest
